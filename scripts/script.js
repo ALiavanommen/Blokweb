d4b0b30984d96472a912cb9bafd8b38fc363a92f
ALiavanommen <144620589+ALiavanommen@users.noreply.github.com>
2023-09-27 15:35:16 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,43 @@
 // JavaScript Document
+
+/*
+
 console.log("hi");
+
+var deEersteP = document.querySelector("li:nth-of-type(1) p");
+
+deEersteP.onclick = maakDeParagraafRood;
+
+/* of */
+
+/*
+deEersteP.addEventListener("click",maakDeParagraafRood);
+
+
+function maakDeParagraafRood();
+deEersteP.classList.toggle("ikBenRood");
+
+*/
+
+// javascript: 
+var headerMenuButton = document.querySelector(".header-menu");
+
+var navMenuButton = document.querySelector(".nav-menu");
+var navMenu = document.querySelector("nav");
+
+ 
+
+headerMenuButton.onclick = toggleMenu;
+navMenuButton.onclick = toggleMenu;
+
+ 
+
+function toggleMenu() {
+
+    headerMenuButton.classList.toggle("open");
+    navMenuButton.classList.toggle("open");
+    navMenu.classList.toggle("open");
+
+}
+
+ 
